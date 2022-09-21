e71f54d8f415e1d3a0c35ed168f6d5a2f0bffcaf
Pierre Wizla <pwizla+github@gmail.com>
2022-09-21 18:02:14 +0200

    Make sidebar hideable

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,6 +73,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true
+        },
+      },
       navbar: {
         logo: {
           alt: 'Strapi Documentation Logo',
